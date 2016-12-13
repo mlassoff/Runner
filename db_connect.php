fabe3cbda8f09a8aa2679f0acc9d7c580a9bed83
Mark Lassoff <Mark Lassoff>
2016-12-12 23:09:45 -0500

    Changed to AWS mysql Instance.

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,17 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "runner_log";
-
+    include('unpw.php');
+    
+    $servername = "teninten.chdo7xi289mh.us-west-2.rds.amazonaws.com:3306";
+    $db_name = "run_tracker";
+    
     $conn = new mysqli($servername, $username, $password);
 
     if($conn->connect_error)
     {
-        die("Connection falied: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
     } else
     {
-       // echo "Connected successully";
+       //echo "Connected successully";
     }
      mysqli_select_db($conn,$db_name);
 ?>
---
+++ unpw.php
@@ -0,0 +1,4 @@
+<?php 
+$username = "mlassoff";
+ $password = "27Hartford";
+?>
